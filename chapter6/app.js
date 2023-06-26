63d2b519fd1a56878686854879a7fc0d0d3ea4c0
jeong922 <hwajeong922@naver.com>
2023-06-26 20:44:18 +0900

    express-2,3

--- chapter6/app.js
+++ chapter6/app.js
@@ -9,11 +9,11 @@ dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev')); // 요청과 응답에 대한 정보 콘솔에 기록, 개발 환경에서 dev, 배포 환경에서 combined
+app.use('/', express.static(path.join(__dirname, 'public'))); // 정적인 파일들을 제공하는 라우터 역할,
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.urlencoded({ extended: false })); // 요청의 본문에 있는 데이터를 해석해서 req.body 객체로 만들어 줌
+app.use(cookieParser(process.env.COOKIE_SECRET)); // 요청에 동봉된 쿠키를 해석해 req.cookies 객체로 만듦
 app.use(
   session({
     resave: false,
@@ -25,7 +25,7 @@ app.use(
     },
     name: 'session-cookie',
   })
-);
+); // 세션 관리용 미들웨어
 
 app.use((req, res, next) => {
   console.log('모든 요청에 다 실행됩니다.');
