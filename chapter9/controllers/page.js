d77a477d1c501aef8e59be4ab5d0dd58d631ca18
jeong922 <hwajeong922@naver.com>
2023-06-30 20:39:43 +0900

    SNS 서비스 - 데이터베이스 셋팅하기

--- chapter9/controllers/page.js
+++ chapter9/controllers/page.js
@@ -1,11 +1,17 @@
+// 미들웨어 컨트롤러
+// 컨트롤러는 res, send, res.json, res.redirect, res.render 등이 존재하는 미들웨어
+
+// 내 정보 페이지 렌더링
 exports.renderProfile = (req, res) => {
   res.render('profile', { title: '내 정보 - NodeBird' });
 };
 
+// 내 정보 페이지 렌더링
 exports.renderJoin = (req, res) => {
   res.render('join', { title: '회원가입 - NodeBird' });
 };
 
+// 메인 페이지 렌더링과 넌적스에 게시글 목록 전달
 exports.renderMain = (req, res, next) => {
   const twits = [];
   res.render('main', {
